bd7e7248d2330317631debf817404250760169dc
Artem Netepin <artnetepin@gmail.com>
2026-07-08 19:57:27 +0300

    Refactor content generation, add verbose response option

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 
+def generate_content(client, messages, is_verbose):
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
+    if not response.usage:
+        raise RuntimeError("No usage information in response")
+
+    if is_verbose:
+        print("User prompt: ", messages[0]["content"])
+        print("Prompt tokens: ", response.usage.prompt_tokens)
+        print("Response tokens: ", response.usage.completion_tokens)
+    print("Response: ")
+    print(response.choices[0].message.content)
+
+
 def main():
     load_dotenv()
     api_key = os.getenv("OPENROUTER_API_KEY")
@@ -13,31 +29,19 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     client = OpenAI(
         base_url="https://openrouter.ai/api/v1",
         api_key=api_key,
     )
-    response = client.chat.completions.create(
-        model="openrouter/free",
-        messages=[
-            {
-                "role": "user",
-                "content": args.user_prompt,
-            }
-        ],
-    )
+    messages = [
+        {"role": "user", "content": args.user_prompt},
+    ]
+    is_verbose = args.verbose
 
-    if response.usage is not None:
-        print("Prompt tokens: ", response.usage.prompt_tokens)
-        print("Response tokens: ", response.usage.completion_tokens)
-
-    else:
-        raise RuntimeError("No usage information in response")
-
-    print("Response: ")
-    print(response.choices[0].message.content)
+    generate_content(client, messages, is_verbose)
 
 
 if __name__ == "__main__":

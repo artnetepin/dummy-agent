4048e98dad55ef0f0a557e6412fd3e5331f5434d
Artem Netepin <artnetepin@gmail.com>
2026-07-08 16:39:50 +0300

    Init openrouter API caht completions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is a dummy AI agent project created for learning purpose.
+The agent is using [OpenRouter](https://openrouter.ai/) on a free tier.
--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
 def main():
-    print("Hello from dummy-agent!")
+    load_dotenv()
+    api_key = os.getenv("OPENROUTER_API_KEY")
+    if not api_key:
+        raise RuntimeError("OPENROUTER_API_KEY not set")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            }
+        ],
+    )
+
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
